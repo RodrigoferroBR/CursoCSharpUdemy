0e7a7640ec8d124b8ec92cd36a06864e7290a4fb
Rodrigo Ferro <rodrigoferrobr@outlook.com>
2017-09-12 00:54:46 -0300

    Outro

---
+++ Secao_02_Aula_17/Produto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Secao_02_Aula_17
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int QuantidadeEmEstoque { get; set; }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Math.Sqrt(QuantidadeEmEstoque * Preco);
+        }
+
+        public string MostrarSaida()
+        {
+            return $"Dados do Produto: {Nome}, R$ {Preco}, Total: R$ {ValorTotalEmEstoque()}";
+        }
+
+        public void EntradaEstoque(int quantidade)
+        {
+            QuantidadeEmEstoque = QuantidadeEmEstoque + quantidade;
+        }
+
+        public void SaidaEstoque(int quantidade)
+        {
+            QuantidadeEmEstoque = QuantidadeEmEstoque - quantidade;
+        }
+
+    }
+}
---
+++ Secao_02_Aula_17/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Secao_02_Aula_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prod = new Produto();
+
+            Console.Write("Digite os dados do Produto: ");
+            prod.Nome = Console.ReadLine();
+
+            Console.Write("Digite o preço do Produto: ");
+            prod.Preco = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite quantidade em Estoque: ");
+            prod.QuantidadeEmEstoque = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
